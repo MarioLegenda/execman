7a7ca2fa86eabb0596dfff89d2cb40a18ddbc73b
mario <marioskrlec222@proton.me>
2025-06-07 16:46:11 +0200

    Removed redundant error

--- emulator.go
+++ emulator.go
@@ -236,7 +236,7 @@ func New(options Options) (Emulator, error) {
 				containerNames[i] = c.Name
 			}
 
-			b := balancer.New(len(containers)*10, containerNames)
+			b := balancer.New(c.WorkerNum, containerNames)
 			b.StartWorkers()
 			e.balancers[c.LangName] = b
 		}(c)
--- errors.go
+++ errors.go
@@ -3,4 +3,3 @@ package execman
 import "errors"
 
 var ContainerCannotBoot = errors.New("container cannot boot up")
-var BalancerNotCreated = errors.New("balancer not created")
