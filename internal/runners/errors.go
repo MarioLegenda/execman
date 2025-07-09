dba87b296b6d5457f10ff39f37e2b59daa63e361
mario <marioskrlec222@proton.me>
2025-07-09 09:43:39 +0200

    Removed unused error on the runners package

--- internal/runners/errors.go
+++ internal/runners/errors.go
@@ -2,7 +2,6 @@ package runners
 
 import "errors"
 
-var WorkerShutdownError = errors.New("worker is shutting down")
 var ExecutionFailed = errors.New("execution failed")
 var CodeExecutionTimeout = errors.New("code execution timed out")
 var FilesystemError = errors.New("filesystem error")
